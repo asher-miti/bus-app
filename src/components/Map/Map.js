2021a23afb0fe904df1283f043c87317f027605c
Asher <asher_3@hotmail.co.uk>
2020-09-04 16:33:33 +0100

    showing selected marker from map in infowindow onclick

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
 
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
-import mapStyles from "./mapStyles";
-import "./Map.css";
+// import mapStyles from "./mapStyles";
 
 // Map settings
 const libraries = ["places"];
@@ -16,9 +15,9 @@ const center = {
   lng: -0.127758,
 };
 
-const options = {
-  styles: mapStyles,
-};
+// const options = {
+//   styles: mapStyles,
+// };
 
 export default function Map() {
   const { isLoaded, loadError } = useLoadScript({
@@ -27,6 +26,22 @@ export default function Map() {
   });
 
   const [markers, setMarkers] = useState([]);
+
+  const onMapClick = useCallback((event) => {
+          setMarkers((current) => [
+            ...current,
+            {
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng(),
+              time: new Date(),
+            },
+          ]);
+        }, [])
+
+  const mapRef = useRef()
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map
+  }, [])
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading page";
@@ -43,18 +58,15 @@ export default function Map() {
         mapContainerStyle={mapContainerStyle}
         zoom={12}
         center={center}
-        options={options}
-        onClick={(event) => {
-          setMarkers((current) => [
-            ...current,
-            {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng(),
-              time: new Date(),
-            },
-          ]);
-        }}
-      ></GoogleMap>
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+      {markers.map((marker) => (
+        <Marker
+        key={marker.time.toISOString()}
+        position={{lat: marker.lat, lng:marker.lng}} />
+      ))}
+      </GoogleMap>
     </div>
   );
 }
