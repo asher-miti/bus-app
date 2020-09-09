7d66a355a0853f6c5616ff3006ed92aaec5ca27e
Asher <asher_3@hotmail.co.uk>
2020-09-09 20:26:42 +0100

    solved issue with displaying estimated time of arrival for each bus at selected stop

--- src/components/BusInfo/BusInfo.js
+++ src/components/BusInfo/BusInfo.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// import styles from './BusInfo.module.css'
+import styles from "./BusInfo.module.css";
 
 const apiKey = process.env.REACT_APP_TRANSPORT_API_KEY;
 const apiId = process.env.REACT_APP_TRANSPORT_API_ID;
@@ -30,6 +30,7 @@ const BusInfo = ({ busStopInfo }) => {
         });
 
         setBusList(result.data.departures);
+        console.log(result.data.departures);
       } catch (error) {
         setIsError(true);
       }
@@ -38,6 +39,11 @@ const BusInfo = ({ busStopInfo }) => {
     getBusTimes();
   }, [busStopInfo.atcocode]);
 
+  // Get current time of day (only minutes) for calculating bus ETA
+  const d = new Date();
+  const n = d.getMinutes();
+  console.log(n);
+
   return (
     <div>
       <h5>{busStopInfo.name}</h5>
@@ -45,10 +51,20 @@ const BusInfo = ({ busStopInfo }) => {
       {busList.all &&
         busList.all.map((departure, i) => (
           <li style={{ listStyleType: "none" }} key={i}>
-            <h6>
-              {departure.best_departure_estimate} --- {departure.line} <small>towards</small>{" "}
-              {departure.direction}
-            </h6>
+            <span style={{ display: "inline-block" }}>
+              <h6 className={styles.busInfo}>
+                <span className={styles.busItem}>{departure.line}</span> <small>towards</small>{" "}
+                {departure.direction}
+              </h6>
+              <h5 className={styles.busInfo}>
+                <strong>
+                  {departure.best_departure_estimate.split(":")[1] - n === 0
+                    ? "Due"
+                    : departure.best_departure_estimate.split(":")[1] - n}
+                </strong>{" "}
+                min
+              </h5>
+            </span>
           </li>
         ))}
     </div>
